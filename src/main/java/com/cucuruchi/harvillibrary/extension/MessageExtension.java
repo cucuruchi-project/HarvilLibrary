dba6f15610416faa053a024df19abc95bd91e32b
cucuruchi-project <156191099+cucuruchi-project@users.noreply.github.com>
2024-02-28 23:03:38 +0900

    feat: HoverClickableMessage 기능추가

--- src/main/java/com/cucuruchi/harvillibrary/extension/MessageExtension.java
+++ src/main/java/com/cucuruchi/harvillibrary/extension/MessageExtension.java
@@ -1,5 +1,9 @@
 package com.cucuruchi.harvillibrary.extension;
 
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 
 import java.util.Arrays;
@@ -22,5 +26,14 @@ public class MessageExtension {
         sendMessage(player, prefix);
         Arrays.stream(subcommands).toList().forEach(
             subcommand -> sendMessages(player, "┗   " + label + " " + subcommand));
+    }
+
+    public static TextComponent HoverClickableMessage(Player player, String message, String hoverText, String clickAction, String clickActionData) {
+        TextComponent textComponent = new TextComponent(message);
+        textComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[] {
+                new TextComponent(hoverText)
+        }));
+        textComponent.setClickEvent(new ClickEvent(ClickEvent.Action.valueOf(clickAction.toUpperCase()), clickActionData));
+        return textComponent;
     }
 }
